64fc6bed4ad2f75688530a23db1293327c3e5d27
Vincentius Daniel Budidharma <131572363+ObscuroLucem@users.noreply.github.com>
2023-04-23 13:42:22 +0700

    Update TembakCalc.py

--- TembakCalc.py
+++ TembakCalc.py
@@ -1,17 +1,13 @@
 """
-Yeah yeah, Python is cringe. Shut up.
-
-Vincentius Daniel Budidharma 12A6/33
-Program ini adalah kalkulator peluang yang terspesialisasi untuk peluang nilai ulangan
-Input: ada berapa soal di ulangan, ada berapa soal yang lu tembak
-Output: list peluang setiap nilai yang possible, peluang lu dapet di atas 75, nilai minimal kalo semua soal yang ditembak salah
-
-Note: yang lu gak tembak diasumsi bener. Jenis soal adalah PG dengan 5 pilihan. Sama kayak PTS :)
+This is a probability calculator for multiple choice test scores
+Input: total number of questions, number of questions you guessed
+Output: list of probabilities for all possible scores, probability to get above 75%, lowest score if all your guessed are incorrect
+Note: Numbers you don't guess are considered correct. The calculator assumes there are only 5 choices per number.
 """
 
 #User Input through console
-TotalSoal = int(input("Total soal:"))
-TotalTembak = int(input("Lu tembak berapa soal:"))
+TotalSoal = int(input("Total number of questions:"))
+TotalTembak = int(input("Number of questions you guessed:"))
 
 #put it in yourself if you want
 """
@@ -19,42 +15,34 @@ TotalSoal = 10
 TotalTembak = 5
 """
 
-#nilai minimal kalo semua tembak salah
+#lowest score if you guessed everything wrong
 def NilaiMinimal():
     Points = TotalSoal - TotalTembak
     NilaiMin = (Points/TotalSoal)*100
     return NilaiMin
 
-#irrelevant
-"""
-#odds of getting 100
-BerkatTuhan = (0.2)**TotalTembak
-print("Peluang lu dapet 100:",BerkatTuhan)
-"""
-
-#List of possible nilai di atas nilai minimal
+#List of possible scores above the lowest score
 ListNilai=[]
 for TotalBener in range(TotalSoal+1):
     if (TotalBener * 100 / TotalSoal) >= NilaiMinimal():
         ListNilai.append(int(TotalBener * 100 / TotalSoal))
 
-#Python doesn't have any built in factorials so I had to learn how to do it from this guy: https://www.javatpoint.com/pyhton-factorial-number
 def Factorial(TheNumber):
     FactorialOutput = 1
     for i in range(1,TheNumber + 1):
         FactorialOutput = FactorialOutput*i
     return FactorialOutput
 
-#Rindu and Ito my beloved
+#12th grade binomial distribution math
 def Peluang(TembakBener):
     Chance = (Factorial(TotalTembak)/(Factorial(TembakBener)*Factorial(TotalTembak-TembakBener)))*(0.2**TembakBener)*(0.8**(TotalTembak-TembakBener))
     return round(Chance*100,5)
 
-#Felina. Print output
+#Prints output
 ListKKM=[]
 TembakBener=0
 print("--------------------------------------------------------------------------------------")
-print("List nilai dan peluangnya:")
+print("List of scores and probabilities:")
 for x in ListNilai:
     print(x, Peluang(TembakBener), "%")
     if NilaiMinimal()+(100*TembakBener/TotalSoal) >= 75: #Peluang di atas 75
@@ -64,5 +52,5 @@ for x in ListNilai:
         break
 
 print("-------------------------------------------------------------------------------------")
-print("Nilai minimal:" + str(NilaiMinimal()))
-print("Peluang di atas 75:", round(sum(ListKKM),2), "%")
+print("Lowest prbably score:" + str(NilaiMinimal()))
+print("Probability your score is above 75%:", round(sum(ListKKM),2), "%")
